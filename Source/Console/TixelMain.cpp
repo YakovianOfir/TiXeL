3fe1bc95f8965f337e098191c001082511d73b49
Ofir Yakovian <ofyakovi@microsoft.com>
2022-01-02 18:08:15 +0200

    [Console]: Create a standalone console application to conduct custom TiXeL workloads

--- Source/Console/TixelMain.cpp
+++ Source/Console/TixelMain.cpp
@@ -1,6 +1,9 @@
 #include "Infrastructure/TixellInfra.hpp"
+#include "Transactions/TixelTxn.hpp"
+#include "DataTypes/TixelAdt.hpp"
 #include <boost/program_options.hpp>
 #include <iostream>
+#include <thread>
 
 enum ReturnValues : int32_t
 {
@@ -10,21 +13,55 @@ enum ReturnValues : int32_t
 	RvCriticalFault = -3
 };
 
-int wmain(int, wchar_t const * const[])
+void ThreadRoutine(const Tixel::Txn::TransactionRoutine& routine)
+{
+    Tixel::Txn::TransactionDispatcher<int, int>::Dispatch(routine);
+}
+
+int wmain(const int argc, wchar_t const * const argv[])
 {
     try
     {
-        TRACE_INF("GVC: [{}]", Tixel::Infra::GlobalVersionClock::Read());
-        TRACE_INF("GVC: [{}]", Tixel::Infra::GlobalVersionClock::AddAndFetch());
-        TRACE_INF("GVC: [{}]", Tixel::Infra::GlobalVersionClock::AddAndFetch());
-        TRACE_INF("GVC: [{}]", Tixel::Infra::GlobalVersionClock::Read());
-        TRACE_INF("GVC: [{}]", Tixel::Infra::GlobalVersionClock::AddAndFetch());
-        TRACE_INF("GVC: [{}]", Tixel::Infra::GlobalVersionClock::Read());
+        Tixel::Infra::CHECK_CMDLINE(argc == 2, "Usage: Tixel <Thread-Count> <Troubleshoot:0|1>");
 
-        return RvSuccess;
+        if (std::stoi(argv[2]) == 1)
+        {
+            Tixel::Infra::EnableTroubleshootMode();
+        }
+
+        const auto threadCount = std::stoi(argv[1]);
+
+        TRACE_INF("Starting workload. (Threads: {})", threadCount);
+
+        auto txnList = Tixel::Txn::TxnList<int, int>::Create();
+
+        std::vector<std::thread> threads;
+
+        for (auto i = 0; i < threadCount; ++i)
+        {
+            threads.emplace_back(std::thread(ThreadRoutine, [&, i]
+            {
+                txnList->Insert(2 * i, 2 * i);
+                txnList->Remove(2 * i + 1);
+            }));
+
+            threads.emplace_back(std::thread(ThreadRoutine, [&, i]
+            {
+                txnList->Insert(2 * i + 1, 2 * i + 1);
+                txnList->Remove(2 * i);
+            }));
+        }
+
+        for (auto& thread : threads)
+        {
+            thread.join();
+        }
+
+        txnList->Print();
     }
-    catch (const std::invalid_argument&)
+    catch (const std::invalid_argument& e)
     {
+        std::cout << "Invalid command line: [" << e.what() << "]" << std::endl;
         return RvInvalidCmdLine;
     }
     catch (const std::exception& e)
