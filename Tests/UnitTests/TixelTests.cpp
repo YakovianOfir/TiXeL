8765ebb60acf1bc3213354094d6ca230014dba0a
Ofir Yakovian <ofyakovi@microsoft.com>
2022-01-02 18:09:00 +0200

    [Tests]: Introduce a lightweight UT infrastructure based on GTest

---
+++ Tests/UnitTests/TixelTests.cpp
@@ -0,0 +1,142 @@
+#include "TixelTests.hpp"
+
+namespace
+{
+    class TixelTests : public ::testing::Test
+    {
+	    public:
+		    virtual ~TixelTests() = default;
+
+	    protected:
+            virtual void SetUp() override
+            {}
+
+            virtual void TearDown() override
+            {}
+
+		public:
+		    static void TxnThreadRoutine(const Tixel::Txn::TransactionRoutine& routine)
+            {
+                Tixel::Txn::TransactionDispatcher<int, int>::Dispatch(routine);
+            }
+    };
+    TEST_F(TixelTests, TxnListInsertCorrectness)
+    {
+        // Arrange
+
+        auto txnList = Tixel::Txn::TxnList<int, int>::Create();
+
+        // Act
+
+        Tixel::Txn::TransactionDispatcher<int, int>::Dispatch([&]
+        {
+    		for (auto i = 0 ; i <= 10; ++i)
+    		{
+                txnList->Insert(i, i);
+    		}
+        });
+
+    	// Assert
+
+        ASSERT_EQ(txnList->Sum(), 55);
+    }
+
+    TEST_F(TixelTests, TxnListContainsCorrectness)
+    {
+        // Arrange
+
+        auto txnList = Tixel::Txn::TxnList<int, int>::Create();
+
+        // Act
+
+        Tixel::Txn::TransactionDispatcher<int, int>::Dispatch([&]
+        {
+            for (auto i = 0; i <= 10; ++i)
+            {
+                txnList->Insert(i, i);
+            }
+        });
+
+        // Assert
+
+        for (auto i = 0; i <= 10; ++i)
+        {
+			ASSERT_TRUE(txnList->Contains(i));
+        }
+    }
+
+    TEST_F(TixelTests, TxnListGetCorrectness)
+    {
+        // Arrange
+
+        auto txnList = Tixel::Txn::TxnList<int, int>::Create();
+
+        // Act
+
+        Tixel::Txn::TransactionDispatcher<int, int>::Dispatch([&]
+        {
+            for (auto i = 0; i <= 10; ++i)
+            {
+                txnList->Insert(i, i);
+            }
+        });
+
+        // Assert
+
+        for (auto i = 0; i <= 10; ++i)
+        {
+            ASSERT_EQ(txnList->Get(i), i);
+        }
+    }
+
+    TEST_F(TixelTests, TxnListRemoveCorrectness)
+    {
+        // Arrange
+
+        auto txnList = Tixel::Txn::TxnList<int, int>::Create();
+
+        // Act
+
+        Tixel::Txn::TransactionDispatcher<int, int>::Dispatch([&]
+        {
+            for (auto i = 0; i <= 10; ++i)
+            {
+                txnList->Insert(i, i);
+                txnList->Remove(i);
+            }
+        });
+
+        // Assert
+
+        ASSERT_TRUE(txnList->Empty());
+    }
+
+    TEST_F(TixelTests, TxnListStressConcurrentTransactions)
+    {
+        // Arrange
+
+        std::vector<std::thread> threads;
+
+        auto txnList = Tixel::Txn::TxnList<int, int>::Create();
+
+        // Act
+
+        for (auto i = 0; i <= 10; ++i)
+        {
+            threads.emplace_back(std::thread(TxnThreadRoutine, [&, i]
+            {
+                txnList->Insert(i, i);
+                txnList->Remove(i);
+            }));
+        }
+
+        for (auto& thread : threads)
+        {
+            thread.join();
+        }
+
+        // Assert
+
+        ASSERT_TRUE(txnList->Empty());
+    }
+}
---
+++ Tests/UnitTests/TixelTests.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+/////////////////////////////////////////////////////////////////
+//
+//   Copyright (c) Ofir Yakovian. All rights reserved.
+//
+//   Module Name:
+//
+//      | TixelTests.hpp |
+//
+//   Author:
+//
+//      | Ofir Yakovian (@ofirkov) |
+//
+//   Environment:
+//
+//		| User Mode |
+//
+/////////////////////////////////////////////////////////////////
+
+#include "gtest/gtest.h"
+#include "Infrastructure/TixellInfra.hpp"
+#include "Transactions/TixelTxn.hpp"
+#include "DataTypes/TixelAdt.hpp"
+
+namespace TixelUnitTests
+{
+    using namespace Tixel::Infra;
+    using namespace Tixel::Adt;
+    using namespace Tixel::Txn;
+    using namespace ::testing;
+}
