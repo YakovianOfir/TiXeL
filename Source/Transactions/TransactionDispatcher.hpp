fd6aeea0aa038d075167051e02ff7a03b8f6b05a
Ofir Yakovian <ofyakovi@microsoft.com>
2022-01-02 18:06:38 +0200

    [TXN]: Formalize the Transaction Dispatcher, to guide the TXN lifecycle

---
+++ Source/Transactions/TransactionDispatcher.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+/////////////////////////////////////////////////////////////////
+//
+//   Copyright (c) Ofir Yakovian. All rights reserved.
+//
+//   Module Name:
+//
+//      | TransactionDispatcher.hpp |
+//
+//   Author:
+//
+//      | Ofir Yakovian (@ofirkov) |
+//
+//   Environment:
+//
+//		| User Mode |
+//
+/////////////////////////////////////////////////////////////////
+
+#include "TransactionRoutine.hpp"
+#include "TxnListTransaction.hpp"
+
+namespace Tixel::Txn
+{
+	template <typename TKey, typename TValue>
+	class TxnListTransaction;
+
+	template <typename TKey, typename TValue>
+	class TransactionDispatcher final : public Infra::NonCopyable
+	{
+		public:
+			explicit TransactionDispatcher() = delete;
+
+		private:
+			friend class TxnListTransaction<TKey, TValue>;
+			using Transaction = TxnListTransaction<TKey, TValue>;
+
+		public:
+			static void Dispatch(const TransactionRoutine& txnRoutine);
+
+		private:
+			static bool TryDispatch(const TransactionRoutine& txnRoutine);
+	};
+
+	template <typename TKey, typename TValue>
+	void TransactionDispatcher<TKey, TValue>::Dispatch(const TransactionRoutine& txnRoutine)
+	{
+		while (!TryDispatch(txnRoutine)) {}
+	}
+
+	template <typename TKey, typename TValue>
+	bool TransactionDispatcher<TKey, TValue>::TryDispatch(const TransactionRoutine& txnRoutine)
+	{
+		try
+		{
+			TRACE_INF("[Dispatcher]: Begin.");
+
+			Transaction::Begin();
+
+			TRACE_VRB("[Dispatcher]: Execute.");
+
+			txnRoutine();
+
+			TRACE_INF("[Dispatcher]: Commit.");
+
+			Transaction::Commit();
+
+			TRACE_INF("[Dispatcher]: Done. (OK)");
+
+			return true;
+		}
+		catch (const Infra::TransactionException&)
+		{
+			TRACE_INF("[Dispatcher]: Rollback.");
+
+			Transaction::Rollback();
+
+			TRACE_INF("[Dispatcher]: Done. (ABORT)");
+
+			return false;
+		}
+		catch (...)
+		{
+			TRACE_ERR("[Dispatcher]: Rollback.");
+
+			Transaction::Rollback();
+
+			TRACE_ERR("[Dispatcher]: Done. (FAULT)");
+
+			throw;
+		}
+	}
+}
---
+++ Source/Transactions/TransactionRoutine.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+/////////////////////////////////////////////////////////////////
+//
+//   Copyright (c) Ofir Yakovian. All rights reserved.
+//
+//   Module Name:
+//
+//      | TransactionRoutine.hpp |
+//
+//   Author:
+//
+//      | Ofir Yakovian (@ofirkov) |
+//
+//   Environment:
+//
+//		| User Mode |
+//
+/////////////////////////////////////////////////////////////////
+
+#include <functional>
+
+namespace Tixel::Txn
+{
+	using TransactionRoutine = std::function<void()>;
+}
