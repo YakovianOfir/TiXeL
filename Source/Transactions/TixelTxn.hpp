09fa572c4d0e285bf92dc05ba3fa956a30d13860
Ofir Yakovian <ofyakovi@microsoft.com>
2022-01-02 18:04:29 +0200

    [TXN]: Introduce an initial library build on top of Infra and ADT

---
+++ Source/Transactions/TixelTxn.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+/////////////////////////////////////////////////////////////////
+//
+//   Copyright (c) Ofir Yakovian. All rights reserved.
+//
+//   Module Name:
+//
+//      | TixelTxn.hpp |
+//
+//   Author:
+//
+//      | Ofir Yakovian (@ofirkov) |
+//
+//   Environment:
+//
+//		| User Mode |
+//
+/////////////////////////////////////////////////////////////////
+
+#include "GlobalVersionClock.hpp"
+#include "TransactionRoutine.hpp"
+#include "TransactionDispatcher.hpp"
+#include "TxnList.hpp"
+#include "TxnListStorage.hpp"
+#include "TxnListTransaction.hpp"
+#include "TxnListOperator.hpp"
+#include "SingleListOperator.hpp"
