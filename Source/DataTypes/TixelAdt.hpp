9a7f2cacec8ea1ac81b0bf8edf4932d21b83ab76
Ofir Yakovian <ofyakovi@microsoft.com>
2022-01-02 18:02:18 +0200

    [ADT]: Introduce the initial library skeleton to later enable the TXN layering

---
+++ Source/DataTypes/TixelAdt.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+/////////////////////////////////////////////////////////////////
+//
+//   Copyright (c) Ofir Yakovian. All rights reserved.
+//
+//   Module Name:
+//
+//      | TixelAdt.hpp |
+//
+//   Author:
+//
+//      | Ofir Yakovian (@ofirkov) |
+//
+//   Environment:
+//
+//		| User Mode |
+//
+/////////////////////////////////////////////////////////////////
+
+#include "IList.hpp"
+#include "Index.hpp"
+#include "IndexKey.hpp"
+#include "IndexNode.hpp"
+#include "SkipList.hpp"
+#include "SkipListMap.hpp"
